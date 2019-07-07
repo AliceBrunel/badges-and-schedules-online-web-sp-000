0db98eff2aef9ead72cd9f8047e22b4ee9968743
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 12:07:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def assign_rooms(array)
   #list of  speaker and assign each to a room 
-  array.each do 
+  array.each do |i|
     #assign each to a room 1-7 and return a list assignment with a string
     assignment = []
     assignment = array.collect{|i| Math.sqrt(i)}
