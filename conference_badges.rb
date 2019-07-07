5ea9254e6d38435412ea503e19eed49d94286ab2
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 13:37:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,6 +29,6 @@ end
 
 def printer(array)
   #print out batch badge and assign room messages
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  batch_badge_creator(array)
+  assign_rooms(array)
 end
