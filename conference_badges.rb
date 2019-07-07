dd30b306b00d81e3b4a2c043467e73546d71ca0a
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 12:14:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,10 @@ end
 
 def assign_rooms(array)
   #list of  speaker and assign each to a room
-  
   array.each do |i|
     #assign each to a room 1-7 and return a list assignment with a string
-    room = array.collect
-    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+    room = 0 
+    room += 1
+    puts "Hello, #{i}! You'll be assigned to room #{room}!"
   end
 end
