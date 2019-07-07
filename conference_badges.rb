e7bf49df5913d54c0e7fe8e64fa3e46dea189873
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 12:04:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(names)
+  "Hello, my name is #{names}"
+end
+
+def assign_rooms(array)
+  #list of  speaker and assign each to a room 
+  array.each do 
+    #do something
+  end
+end
