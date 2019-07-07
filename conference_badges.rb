f00820febedb6d743fd5c1f26bf36deceaac31b0
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 12:12:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,14 @@
 # Write your code here.
-def badge_maker(names)
-  "Hello, my name is #{names}"
+def badge_maker(name)
+  "Hello, my name is #{name}"
 end
 
 def assign_rooms(array)
   #list of  speaker and assign each to a room
-  assigned_room = []
+  
   array.each do |i|
     #assign each to a room 1-7 and return a list assignment with a string
-    assigned_room = array.collect
-    puts ""
+    room = array.collect
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
